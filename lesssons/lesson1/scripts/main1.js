f8681855ff348e68c0bcae8ab3050d18f5e805fe
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-13 22:31:46 +0300

    ок

---
+++ lesssons/lesson1/scripts/main1.js
@@ -0,0 +1,24 @@
+// console.log("Hello World!");
+//number
+// string
+// boolean
+
+// var a; // це декларація зміни-->
+// a = 'Hello' //  присвоєння значення-ініціалізована
+
+let a='hello okten'
+{
+    let a = 'HELLO OKTEN';
+    var b ='hello world'
+}
+console.log(b); // доступна за межами блока
+console.log(a);// доступна тільки в межі блока
+
+const PI=3.14
+console.log(PI);
+// PI=`123` заборонено перевизначати
+
+let c='qwert';
+console.log(c);
+c ='hello Nata';
+console.log(c);  // можна перевизначити
