abde4c74f6e49e501e954a17ac27f96d08e3e581
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-14 22:30:00 +0300

    ок

---
+++ lesssons/lesson1/main2.js
@@ -0,0 +1,12 @@
+let  a=' hello okten';
+// a = '5';
+console.log( typeof a , a); // typeof-визначення типу даних
+
+let b = 123456;
+console.log( typeof b, b );  // number блакитний
+
+let c=true// використовується в логічних розгалуженнях // false
+console.log( c );
+
+
+
