db04759d4aea041d6e9335eb4575ff4fedd57667
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-17 20:28:55 +0300

    ок

---
+++ lesssons/lesson1/number-operestion-их/main.js
@@ -0,0 +1,20 @@
+let a =100500;
+console.log(a);
+let b = 3.12345678; //
+console.log(b);
+
+console.log(10+20);
+console.log(20-10);
+console.log(10*20);
+console.log(10/20);
+console.log(11 % 3); // ділення з остачою (тобто пише відповідь 2)
+
+console.log(0.1+0.2);
+
+let big= BigInt(12345678911234561223);
+console.log(big);
+
+let n1 =100;
+let n2 =5;
+console.log(n1+ n2);
+
