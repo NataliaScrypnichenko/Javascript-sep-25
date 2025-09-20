b2f3755753b9f3e4a571d3ea9a7fc4bc42c96375
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-20 20:29:24 +0300

    виконано

---
+++ HW/hw-2/#LARqoUj5I/main.js
@@ -0,0 +1,19 @@
+//Поля об’єкту : title ,pageCount, genre.
+
+let book1={
+    title:"JavaScript",
+    pageCount: 400,
+    gener:"science",
+};
+let book2={
+    title:"Memory",
+    pageCount: 200,
+    gener:"psychology",
+};
+let book3 = {
+    title: "Joan of Arc",
+    pageCount: 157,
+    gener: "historical",
+};
+
+
