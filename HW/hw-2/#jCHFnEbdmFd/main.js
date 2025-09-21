f4f72919d5f4de0dcb5557d0890d81304037a229
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-21 22:37:53 +0300

    make

---
+++ HW/hw-2/#jCHFnEbdmFd/main.js
@@ -0,0 +1,23 @@
+let users = [
+    {name: 'vasya', username: "erty", password: 1233 },
+    {name: 'vasya', username: "erty", password: '3456jk' },
+    {name: 'vasya', username: "erty", password: '12eety566' },
+    {name: 'olya', username: "erty", password: 12566 },
+    {name: 'anna', username: "erty", password: 1566 },
+    {name: 'vasya', username: "erty", password: '12eety096' },
+    {name: 'vasya', username: "erty", password: 'ghg12eety566' },
+    {name: 'masha', username: "erty", password: '12eety5jhjk66' },
+    {name: 'ket', username: "erty", password: '12eety5gh66' },
+    {name: 'pep', username: "erty", password: '12eety566fdgfj' },
+
+];
+console.log(users [0].password);
+console.log(users [1].password);
+console.log(users [2].password);
+console.log(users [3]['password']);
+console.log(users [4]['password']);
+console.log(users [5]['password']);
+console.log(users [6]['password']);
+console.log(users [7]['password']);
+console.log(users [8]['password']);
+console.log(users [9]['password']);
