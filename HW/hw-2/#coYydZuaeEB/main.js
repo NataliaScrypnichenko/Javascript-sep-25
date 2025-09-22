c181d3a86ac5a330980598058d372982a45d6ed6
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-09-22 22:18:27 +0300

    виконала

---
+++ HW/hw-2/#coYydZuaeEB/main.js
@@ -0,0 +1,33 @@
+ let weatherForWeek = [
+     {dayWeek:'Monday', morning: 22, day: 30, evening:20},
+     {dayWeek:'Tuesday', morning: 18, day: 24, evening:10},
+     {dayWeek:'Wednesday', morning: 12, day: 20, evening:10},
+     {dayWeek:'Thursday', morning: -3, day: 15, evening:11},
+     {dayWeek:'Friday', morning: 6, day: 6, evening:1},
+     {dayWeek:'Saturday', morning: 7, day: 13, evening:10},
+     {dayWeek:'Sunday', morning: 12, day: 12, evening:2},
+ ];
+console.log(weatherForWeek[0]);
+console.log(weatherForWeek[1]);
+console.log(weatherForWeek[2]);
+console.log(weatherForWeek[3]);
+console.log(weatherForWeek[4]);
+console.log(weatherForWeek[5]);
+console.log(weatherForWeek[6]);
+
+let weather=[
+    [12, 6,10],
+    [6,7,9],
+    [8,9,10],
+    [-2, 7,8],
+    [-3,-3,0],
+    [0, -1, 2],
+    [5,2,0]
+];
+console.log(weather[0]);
+console.log(weather[1]);
+console.log(weather[2]);
+console.log(weather[3]);
+console.log(weather[4]);
+console.log(weather[5]);
+console.log(weather[6]);
