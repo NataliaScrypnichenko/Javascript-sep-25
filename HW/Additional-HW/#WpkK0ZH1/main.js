a7fe3eda1a31fd6936772c123d6e0f5f1a43101c
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-03 00:09:44 +0300

    виконала

---
+++ HW/Additional-HW/#WpkK0ZH1/main.js
@@ -0,0 +1,15 @@
+//–створити масив з:
+// – та вивести його в консоль
+
+// – з 5 числових значень
+let arr1;
+arr1 = [23,56,788,999,6];
+console.log(arr1);
+// – з 5 стічкових значень
+let arr2;
+arr2=['word','book','also','name','television'];
+console.log(arr2);
+//// – з 5 значень стрічкового, числового та булевого типу
+let arr;
+arr=['word','book',234,true,false];
+console.log(arr);
