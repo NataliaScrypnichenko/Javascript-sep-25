ddb5df9bff32e5dbeb2d0e7a5d2d09ab70be5506
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-03 15:52:20 +0300

    виконано

---
+++ HW/додаткові-HW/#yHAwJOyiC/main.js
@@ -0,0 +1,11 @@
+// – Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+    let arrNumber;
+    arrNumber=[34,678,78,90,1,2,34,53,56,89];
+
+for (let i = 0; i < arrNumber.length; i++) {
+
+    let arrNumberElement = arrNumber[i];
+
+    console.log(arrNumberElement);
+
+};
