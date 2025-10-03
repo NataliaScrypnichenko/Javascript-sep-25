ff73a546ef1708caaa98506912b2320004afc4f5
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-03 16:49:19 +0300

    виконано

---
+++ HW/додаткові-HW/#Bm76xmg/main.js
@@ -0,0 +1,8 @@
+// – Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+
+let arr=[23,89,57,'asdf','book',-34,true,false,'Anna',"Hello"];
+let i=0;
+while(i < arr.length){
+   console.log(arr[i]);
+   i++;
+}
