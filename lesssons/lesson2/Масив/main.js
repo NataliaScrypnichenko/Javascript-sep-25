424ef84969efe585d98b08237124195c5fb6d340
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-03 15:41:02 +0300

    ok

--- lesssons/lesson2/Масив/main.js
+++ lesssons/lesson2/Масив/main.js
@@ -7,7 +7,7 @@ console.log(arr[1]);
 console.log(arr[4]);
 // довжина масива
 console.log(arr.length);//7
-//перевизначення- замінити елемента
+// перевизначення- замінити елемента
 arr[0]='new value';
 console.log(arr);
 // останній індекс це довжина мінус 1
@@ -16,5 +16,5 @@ arr[7]= 'okten';
 console.log(arr);//[ 'new value', 234, 345, -123, 0, true, 'asdfgh', 'okten' ]//8 -елементів,7-індекс
 //+ нового елемента не рахуючи їх
 arr[arr.length]=1234567;
-console.log(arr); 
+console.log(arr);
 
