6a37d71a3d2231a076bad1c774d0a77c8319b316
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-03 16:41:13 +0300

    виконано

---
+++ HW/додаткові-HW/#GamKju89ob/main.js
@@ -0,0 +1,8 @@
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+
+let users;
+users = ['Alvina','Badger','Angela','Allan','Fabian','Giles','Pety', 'Norma','Alvina','Badger']
+console.log(users);
+for (let user of users) {
+    console.log(user);
+}
