fdf02be7519bebe0d1ff32075f48863848ebccc9
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-10-09 21:55:28 +0300

    виконала

--- HW/додаткові-HW/#reLkOkTB29Q/main.js
+++ HW/додаткові-HW/#reLkOkTB29Q/main.js
@@ -54,3 +54,67 @@ for (let book of books) {
 };
  console.log('найбільша книга:', bigBook);
 
+// – знайти книжку/ки з найбільшою кількістю жанрів
+let bigBookWithGenre=0;
+for (let i = 0; i < books.length; i++) {
+    let countGenre;
+    if (typeof books[i].genres === 'object'){ //genres є масивом (бо масив — це об’єкт у JS)
+         countGenre= books[i].genres.length;
+    }else{
+        countGenre = 1;//якщо це не масив, тоді вважаю, що там один жанр
+    }
+    if (bigBookWithGenre < countGenre){
+        bigBookWithGenre = countGenre;
+
+    }
+
+};
+console.log('Найбільша кількість жанрів:',bigBookWithGenre);
+// – знайти книжку/ки з найдовшою назвою
+// let maxTitleLength = 0;
+// for (let i = 0; i < books.length; i++) {
+//     if (!maxTitleLength) {
+//         maxTitleLength = books[i];
+//     }
+//     if(books[i].genres.length > maxTitleLength.genres.length){
+//         maxTitleLength = books[i].genres.length;
+//     }
+// }
+// console.log(maxTitleLength);
+let maxTitleLength = 0;
+for (let i = 0; i < books.length; i++) {
+    if (books[i].title.length > maxTitleLength) {
+        maxTitleLength = books[i].title.length;
+    }
+    if (books[i].title.length === maxTitleLength) {
+        console.log('Книжки з найдовшою назвою:')
+        console.log('-', books[i].title);
+    }
+};
+
+// // Книжки, які писали 2 автори
+for (let i = 0; i < books.length; i++) {
+    let count = 0;
+    if (typeof books[i].authors === 'object') {
+        count = books[i].authors.length;
+    } else {
+        count = 1;
+    }
+    if (count === 2) {
+       console.log('Книжки, які писали 2 автори:' ,books[i].authors);
+    }
+};
+//- Книжки, які писав 1 автор
+
+for (let i = 0; i < books.length; i++) {
+    let count = 0;
+    if (typeof books[i].authors === 'object') {
+        count = books[i].authors.length;
+    } else {
+        count = 1;
+    }
+    if (count === 1) {
+        console.log('Книжки, які писав 1 автор:', books[i].title);
+    }
+};
+
---
+++ HW/додаткові-HW/різні/task1.js
@@ -0,0 +1,43 @@
+// 1. Створити пустий масив та :
+//
+//         a. заповнити його 50 парними числами за допомоги циклу.
+//
+//         b. заповнити його 50 непарними числами за допомоги циклу.
+//
+//         c. Заповнити масив 20-ма рандомними числами. (Google: Generate random number JS)
+//
+// d. Заповнити масив 20-ма рандомними числами в діапазоні від 8 до 732 (Google: Generate random number JS)
+//
+// // a. заповнити його 50 парними числами за допомоги циклу.
+let arr=[];
+for (let i=0; i<50; i++){
+   arr[i]=i*2;
+}
+console.log(arr);
+// // b. заповнити його 50 непарними числами за допомоги циклу.
+let numbers=[];
+for (let i=0; i<50; i++){
+    numbers[i]=i*2+1;
+}
+console.log(numbers);
+// //   c. Заповнити масив 20-ма рандомними числами. (Google: Generate random number JS)
+let arr1 = [];
+
+for (let i = 0; i < 20; i++) {
+    let randomNum = Math.floor(Math.random() * 100); // від 0 до 99
+    arr1[i] = randomNum;
+}
+
+console.log(arr1);
+// // d. Заповнити масив 20-ма рандомними числами в діапазоні від 8 до 732 (Google: Generate random number JS)
+
+let arr2 = [];
+for (let i = 0; i<20; i++) {
+
+    let randomNumber = Math.floor(Math.random() * (732 - 8+1)) + 8;
+
+    arr2[i] = randomNumber;
+}
+console.log(arr2);
+
+
