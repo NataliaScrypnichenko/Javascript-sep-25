132149e34cac7dd1e269d809ddac6fb8816ce385
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-01-23 20:48:53 +0200

    ok

---
+++ lesssons/lesson7/класи-та-наслідування/class.js
@@ -0,0 +1,14 @@
+
+class User{
+
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+   greeting(){
+        return `Hello,my name is ${this.name} ` ;
+   }
+  work(){
+        return `i have work`;
+  }
+}
