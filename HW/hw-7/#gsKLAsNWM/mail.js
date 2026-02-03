534ae45a09001f5c1fc1e8d1f42aeb1664cd6617
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-02-03 21:31:03 +0200

    виправила

--- HW/hw-7/#gsKLAsNWM/mail.js
+++ HW/hw-7/#gsKLAsNWM/mail.js
@@ -5,7 +5,7 @@
 
 Array.prototype.myForEach = function (callback) {
     for (let i = 0; i < this.length; i++) {
-        callback(this[i]);
+        callback(this[i],i,this);
     };
 };
 let users = [
