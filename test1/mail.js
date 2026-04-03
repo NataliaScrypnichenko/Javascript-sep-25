cfb6b352229e707a3079f5c67c378ebdf185b8ec
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-04-03 18:06:03 +0300

    ще робити

--- test1/mail.js
+++ test1/mail.js
@@ -1,8 +1,3 @@
-// <input id="nameValue" name="ifoNameValue" placeholder="Name=Value"/>
-// <button id="buttonAdd">Add</button>
-// <p id="error" className="error"></p>
-// <div id="buttonInfo" className="buttonInfo">
-
 
 let input = document.getElementById("nameValue");
 let divInfo = document.getElementById("info");
@@ -21,21 +16,12 @@ buttonAdd.onclick = function () {
     if (matchResult) {
         // якщо правильний
         let p = document.createElement("p");
-        // створюю checkbox
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.classList.add('checkbox');
-        //створимо div для стилізації p and checkbox
-
-        let divStyle = document.createElement("div");
-        divStyle.classList.add('div_style');
-        divStyle.append(p, checkbox);
 
         // додаю інформацію
-        p.innerText = matchResult[1] + " = " + matchResult[2] + " ";
-        // p.appendChild(checkbox);
+        p.innerText = matchResult[1] + " = " + matchResult[2] ;
 
-        divInfo.appendChild(divStyle);
+
+        divInfo.appendChild(p);
         // очищаю повідомлення про помилку
         error.innerText = "";
     } else {
@@ -51,10 +37,74 @@ buttonAdd.onclick = function () {
     let buttonSortValue= document.getElementById("buttonSortValue");
     let buttonDelete=document.getElementById("buttonDelete");
 
-    buttonSortName.onclick = function (ev) {
-         let arr=[];
-         let items=divInfo.children
-         arr.push(items[i]);
 
 
+buttonSortName.onclick = function () {
+//створюю масив для сортування
+    let arr = [];
+    let items = divInfo.children;
+
+    for (let i = 0; i < items.length; i++) {
+        arr.push(items[i]);
     }
+   //сортую значення
+    arr.sort(function(a,b){
+         // створюю зміну де беру тільки значення
+        let nameA=a.innerText.split('=')[0].trim();
+         let nameB =b.innerText.split('=')[0].trim();
+
+     if (nameA>nameB) {
+         return 1;
+      }
+     if (nameA<nameB){
+         return -1;
+     }
+     if (nameA === nameB) {
+         return 0;
+     }
+
+    });
+    // очищаю зразу поле
+    divInfo.innerHTML = "";
+    // записую назад послідовно значення
+    for (let i = 0; i < arr.length; i++) {
+        divInfo.appendChild(arr[i]);
+    }
+
+}
+
+
+buttonSortValue.onclick = function () {
+    let arr = [];
+    let items = divInfo.children;
+
+    for (let i = 0; i < items.length; i++) {
+        arr.push(items[i]);
+    }
+
+    arr.sort(function(a,b){
+        let valueA=a.innerText.split('=')[1].trim();
+        let valueB=b.innerText.split('=')[1].trim();
+
+        return valueB - valueA ;
+
+    })
+
+    divInfo.innerHTML = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        divInfo.appendChild(arr[i]);
+    }
+}
+    // // створюю checkbox
+//         // let checkbox = document.createElement("input");
+//         // checkbox.type = "checkbox";
+//         // checkbox.classList.add('checkbox');
+//         // p.appendChild(checkbox);
+//         //створимо div для стилізації p and checkbox
+//
+//         // let divStyle = document.createElement("div");
+//         // divStyle.classList.add('div_style');
+//         // divStyle.append(p, checkbox);
+// p.appendChild(checkbox);
+// p.classList.add('p_checkbox');
